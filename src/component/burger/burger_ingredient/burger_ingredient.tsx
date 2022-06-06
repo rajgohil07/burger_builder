494b2ec9f6a81cbe053e5b8e8f0a0fc74dbfa0dd
rajgohil07 <raj.famous007@gmail.com>
2022-06-06 12:53:00 +0530

    have added switch case in burger ingredient component

--- src/component/burger/burger_ingredient/burger_ingredient.tsx
+++ src/component/burger/burger_ingredient/burger_ingredient.tsx
@@ -1,1 +1,37 @@
-export const BurgerIngredient = () => {};
+import { BurgerIngredientType } from "../../../types/burger_ingredient_types";
+
+export const BurgerIngredient = ({
+  types,
+}: {
+  types: BurgerIngredientType;
+}) => {
+  let ingredient;
+  switch (types) {
+    case BurgerIngredientType.BreadBottom:
+      ingredient = <div className="BreadBottom"></div>;
+      break;
+    case BurgerIngredientType.BreadTop:
+      ingredient = (
+        <div className="BreadTop">
+          <div className="Seeds1"></div>
+          <div className="Seeds2"></div>
+        </div>
+      );
+      break;
+    case BurgerIngredientType.Meat:
+      ingredient = <div className="Meat"></div>;
+      break;
+    case BurgerIngredientType.Cheese:
+      ingredient = <div className="Cheese"></div>;
+      break;
+    case BurgerIngredientType.Salad:
+      ingredient = <div className="Salad"></div>;
+      break;
+    case BurgerIngredientType.Bacon:
+      ingredient = <div className="Bacon"></div>;
+      break;
+    default:
+      break;
+  }
+  return ingredient;
+};
--- src/types/burger_ingredient_types.ts
+++ src/types/burger_ingredient_types.ts
@@ -2,4 +2,7 @@ export enum BurgerIngredientType {
   BreadBottom = 1,
   BreadTop = 2,
   Meat = 3,
+  Cheese = 4,
+  Salad = 5,
+  Bacon = 6,
 }
