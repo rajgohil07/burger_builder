8ab5c600e8cf9f322746b0bf244e6274ffaa665b
rajgohil07 <raj.famous007@gmail.com>
2022-06-29 13:25:27 +0530

    have added checkoutSummary componnet and checkout container

---
+++ src/component/order/checkout_summary/checkout_summary.tsx
@@ -0,0 +1,25 @@
+import { Button } from "@mui/material";
+import { Burger } from "../../burger/burger";
+
+export const CheckoutSummary = ({ burgerOption }: { burgerOption: any }) => (
+  <div className="checkoutSummaryOuterDiv">
+    <h1>We hope it tastes well!</h1>
+    <div className="checkoutSummary">
+      <Burger burgerOption={burgerOption} />
+    </div>
+    <Button
+      style={{ textTransform: "uppercase" }}
+      variant="contained"
+      color="success"
+    >
+      Continue
+    </Button>
+    <Button
+      style={{ textTransform: "uppercase" }}
+      variant="outlined"
+      color="error"
+    >
+      Cancel
+    </Button>
+  </div>
+);
---
+++ src/container/checkout/checkout.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { CheckoutSummary } from "../../component/order/checkout_summary/checkout_summary";
+import { IBurgerOptionType } from "../../types/burger_option_types";
+
+export const Checkout = () => {
+  // set burger option state
+  const [getCheese, setCheese] = useState<number>(0);
+  const [getSalad, setSalad] = useState<number>(0);
+  const [getMeat, setMeat] = useState<number>(0);
+  const [getBacon, setBacon] = useState<number>(0);
+
+  const burgerOption: IBurgerOptionType = {
+    cheese: getCheese,
+    bacon: getBacon,
+    salad: getSalad,
+    meat: getMeat,
+  };
+  return (
+    <div>
+      <CheckoutSummary burgerOption={burgerOption} />
+    </div>
+  );
+};
