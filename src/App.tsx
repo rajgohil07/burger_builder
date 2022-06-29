8e83b97e63d4d14e79414c0d5129022767328141
rajgohil07 <raj.famous007@gmail.com>
2022-06-29 13:37:18 +0530

    have setup the base URL

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { BrowserRouter } from "react-router-dom";
 import { Layout } from "./component/layout/layout";
 
 const App = () => {
-  return <Layout />;
+  const baseURL =
+    process.env.NODE_ENV === "production" ? "/burger_builder" : "/";
+  return (
+    <BrowserRouter basename={baseURL}>
+      <Layout />
+    </BrowserRouter>
+  );
 };
 
 export default App;
